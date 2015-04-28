dbe2011f5034abda2c1de16243b1b840c726c5e2
NemoGara <garaheaven@163.com>
2015-04-28 09:58:17 +0800

    frist version

---
+++ src/com/testcontact/ScreenSize.java
@@ -0,0 +1,23 @@
+package com.testcontact;
+
+public class ScreenSize {
+	// screen width and height
+	public static int width;
+	public static int height;
+
+	public static int getWidth() {
+		return width;
+	}
+
+	public static void setWidth(int width) {
+		ScreenSize.width = width;
+	}
+
+	public static int getHeight() {
+		return height;
+	}
+
+	public static void setHeight(int height) {
+		ScreenSize.height = height;
+	}
+}
